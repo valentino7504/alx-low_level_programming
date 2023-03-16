7939edc3fec80987f7b8d0a3d610ce0b35a80a95
Edwin Ade <edwinmbonyjr@gmail.com>
2023-03-16 14:46:50 +0100

    Solved task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main: prints text in task 5
+* Return: 0 if program prints text
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
