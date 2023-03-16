2ba3de5eb984d1cdab7be7d770d578f80cac275b
Edwin Ade <edwinmbonyjr@gmail.com>
2023-03-16 14:44:18 +0100

    Solved task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - This function outputs the text in task 4
+ * Return: 0 if program successfully prints required text
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
