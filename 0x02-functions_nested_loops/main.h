fd9b05678e9bb7b508c4726429628f8b86d3e4e8
Edwin Ade <edwinmbonyjr@gmail.com>
2023-03-21 16:11:54 +0100

    Solved task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: parameter to compare
+ * Return: 0 if program runs
+ */
+int _putchar(char c);
+
+#endif
