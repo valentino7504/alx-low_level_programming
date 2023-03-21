d56376fe9539746aa9301fa55af2f5f9c90b6c3f
Edwin Ade <edwinmbonyjr@gmail.com>
2023-03-21 22:46:53 +0100

    Solved optional task 3

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - gets fibonacci number
+ * @n: the position in fibonacci series
+ * Return: the fibonacci number
+ */
+int fibonacci(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (fibonacci(n - 2) + fibonacci(n - 1));
+	}
+}
+
+/**
+ * main - main function for fibonacci
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d", fibonacci(i));
+		if (i != 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
