e4fa93558d8a935c5500ada32c1e4e785704e70d
Edwin Ade <edwinmbonyjr@gmail.com>
2023-03-21 20:23:35 +0100

    Solved task 9 and updated main.h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - print times table from 0 to 9
+ */
+
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = j * i;
+			if (product < 10)
+			{
+				_putchar(product + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
