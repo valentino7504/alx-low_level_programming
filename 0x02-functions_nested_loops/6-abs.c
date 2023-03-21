c62b65b1d829ca9c2e9a721dd9b4f51d2829bb09
Edwin Ade <edwinmbonyjr@gmail.com>
2023-03-21 17:38:59 +0100

    solved task 6 on abs. function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of an int
+ * @n: number to be checked
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	return ((n < 0) ? (-1 * n) : (n));
