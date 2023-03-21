8905b9bfdc6b3190a3de621fccfae1a735a08926
Edwin Ade <edwinmbonyjr@gmail.com>
2023-03-21 16:26:12 +0100

    Updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,9 +3,26 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: parameter to compare
+ * @c: parameter to print
  * Return: 0 if program runs
  */
 int _putchar(char c);
+
+/**
+ * print_alphabet - prints alphabet
+ */
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ */
+void print_alphabet_x10(void);
+
+/**
+ * _islower - checks if a character is lowercase
+ * @c: parameter to check
+ * Return: 1 if lower and 0 if not
+ */
+int _islower(int c);
 
 #endif
