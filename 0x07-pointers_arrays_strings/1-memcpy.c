417262be74dbfd8db1b116afd2d3fdf160379356
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-03 13:29:01 +0100

    Corrected task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,6 +5,7 @@
  * @dest: the destination string
  * @src: the source string
  * @n: the number of bytes to be copied
+ * Return: the destination string
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
