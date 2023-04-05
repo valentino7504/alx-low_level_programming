437ba5ae92f31bee5ba5466c84e883b0957694e9
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-05 13:15:20 +0100

    Solved task 0, created main.h and README

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: the string
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - prints a character to the output stream
+ * @c: the character to be printed
+ * Return: the printed character
+ */
+int _putchar(char c);
+
+/**
+ * _strlen - find the length of a string
+ * @s: the string
+ * Return: the length of the string
+ */
+int _strlen(char *s);
+
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: the string
+ */
+void _puts_recursion(char *s);
+#endif
