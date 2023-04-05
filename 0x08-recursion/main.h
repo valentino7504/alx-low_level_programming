b12d7a798932dc35f980a73f1587d57debc1bb76
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-05 13:35:48 +0100

    Solved task 1 and updated main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -20,4 +20,10 @@ int _strlen(char *s);
  * @s: the string
  */
 void _puts_recursion(char *s);
+
+/**
+ * _print_rev_recursion - prints a string in reverse using recursion
+ * @s: the string to be printed
+ */
+void _print_rev_recursion(char *s);
 #endif
