80cee5893a4887ce206c9b983fc4ffb34dd331b7
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-07 16:11:32 +0100

    Solved task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function body
+ * @argc: the number of command line arguments
+ * @argv: the command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, result;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		result *= atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
