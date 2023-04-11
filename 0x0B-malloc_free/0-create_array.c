137e086a68076504fad9fe3fa119b5bfebec0be2
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-11 19:56:22 +0100

    Solved task 0, created header and README

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars and intialises it with c
+ * @size: the size of the array
+ * @c: the character
+ * Return: pointer to the array or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return ((void *)0);
+	}
+	arr = malloc(sizeof(char) * size);
+	if (arr == 	NULL)
+	{
+		return ((void *)0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Solution to all the problems in ALX 0x0B-malloc_free
