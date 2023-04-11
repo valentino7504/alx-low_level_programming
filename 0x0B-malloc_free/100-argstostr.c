4643633715e2c49b3eb13ee803237c96bb263c75
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-12 00:37:30 +0100

    Solved extra task 1 and updated main.h

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "strlen.c"
+/**
+ * argsttostr - concatenates args into a string
+ * @ac: the number of args
+ * @av: the args
+ * Return: a concat string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	int length = 1;
+	char *concatenated;
+	int stop_point;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		length += _strlen(av[i]);
+	}
+	length += ac;
+	concatenated = malloc(sizeof(char) * length);
+	if (concatenated == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			concatenated[stop_point] = av[i][j];
+			stop_point++;
+		}
+		concatenated[stop_point] = '\n';
+		stop_point++;
+	}
+	concatenated[stop_point] = '\0';
+	return (concatenated);
+}
