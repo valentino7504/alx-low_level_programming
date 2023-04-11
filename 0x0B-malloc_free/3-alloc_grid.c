f8efb0e7f9fb432c8b764c950fb55e28f5ded086
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-11 22:58:37 +0100

    Tried task 3 again

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,25 +7,38 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **matrix, i;
+	int i, j;
+	int **grid;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	matrix = malloc(sizeof(int *) * height);
-	if (matrix == NULL)
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
-		matrix[i] = malloc(sizeof(int) * width);
-		if (matrix[i] == NULL)
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
 		{
-			free(matrix);
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
 			return (NULL);
 		}
+
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
 	}
-	return (matrix);
+
+	return (grid);
 }
