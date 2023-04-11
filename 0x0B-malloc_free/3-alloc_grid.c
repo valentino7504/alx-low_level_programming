897350eccb7ee1298633dc3110eab7a45161c029
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-11 22:33:38 +0100

    Solved task 3 and updated main.h

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2d array of ints
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: the grid or NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int **matrix, i;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	matrix = malloc(sizeof(int *) * height);
+	if (matrix == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		matrix[i] = malloc(sizeof(int) * width);
+		if (matrix[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (matrix);
+}
