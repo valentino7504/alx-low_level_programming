a0fb66871cd86015a27ac1d6184af3de8c502a4d
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-17 14:49:22 +0100

    Solved task 1 and updated dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - initialises an instance of the dog structure
+ * @d: the dog instance
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
