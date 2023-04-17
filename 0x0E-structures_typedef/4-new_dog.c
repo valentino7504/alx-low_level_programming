e72dd6de459b742123f9bf523d0678280c11fbdc
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-17 16:10:28 +0100

    Resolved task 4 issues

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -54,12 +54,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	fresh_dog->name = malloc((sizeof(char) * _strlen(name)) + 1);
 	if (fresh_dog->name == NULL)
 	{
+		free(fresh_dog);
 		return (NULL);
 	}
 	_strcpy(name, fresh_dog->name);
 	fresh_dog->owner = malloc((sizeof(char) * _strlen(owner)) + 1);
 	if (fresh_dog->owner == NULL)
 	{
+		free(fresh_dog->name);
+		free(fresh_dog);
 		return (NULL);
 	}
 	_strcpy(owner, fresh_dog->owner);
