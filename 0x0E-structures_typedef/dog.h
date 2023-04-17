ae23c2bd606f3966d65116b2a3f7c2e41bd9107a
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-17 14:41:41 +0100

    Resolved betty issues

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a structure to create dogs
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description - A simple dog-creating structure
+ */
 struct dog
 {
 	char *name;
