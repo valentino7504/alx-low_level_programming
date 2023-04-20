d44c3432068fa30d81abf4c43b63038e3ac30c19
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-20 21:29:57 +0100

    Solved task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_all - prints different formats
+ * @format: the formatted string
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char *string;
+	int i;
+
+	va_start(ap, format);
+	i = 0;
+	while (*(format + i))
+	{
+		if (*(format + i) == 'c')
+		{
+			printf("%c", va_arg(ap, int));
+		}
+		else if (*(format + i) == 'i')
+		{
+			printf("%d", va_arg(ap, int));
+		}
+		else if (*(format + i) == 'f')
+		{
+			printf("%f", va_arg(ap, double));
+		}
+		else if (*(format + i) == 's')
+		{
+			string = va_arg(ap, char *);
+			if (string == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			{
+				printf("%s", string);
+			}
+		}
+		else
+		{
+			i++;
+			continue;
+		}
+		if (*(format + i + 1) != '\0')
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
