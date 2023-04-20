6aaef2b510380b8a5c030db7fb473215a0b291d8
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-20 21:45:44 +0100

    Resolved issues with task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,36 +10,36 @@ void print_all(const char * const format, ...)
 	va_list ap;
 	char *string;
 	int i = 0;
+	char current;
 
 	va_start(ap, format);
 	while (*(format + i))
 	{
-		if (*(format + i) == 'c')
-		{
-			printf("%c", va_arg(ap, int));
-		}
-		else if (*(format + i) == 'i')
-		{
-			printf("%d", va_arg(ap, int));
-		}
-		else if (*(format + i) == 'f')
-		{
-			printf("%f", va_arg(ap, double));
-		}
-		else if (*(format + i) == 's')
-		{
-			string = va_arg(ap, char *);
-			if (string == NULL)
-				printf("(nil)");
-			else
-			{
-				printf("%s", string);
-			}
-		}
-		else
+		current = *(format + i);
+		if (current != 'c' && current != 'i'
+		&& current != 'f' && current != 's')
 		{
 			i++;
 			continue;
+		}
+		if (*(format + i) == 'c')
+			printf("%c", va_arg(ap, int));
+		if (*(format + i) == 'i')
+			printf("%d", va_arg(ap, int));
+		if (*(format + i) == 'f')
+			printf("%f", va_arg(ap, double));
+		if (*(format + i) == 's')
+		{
+			string = va_arg(ap, char *);
+			if (string == NULL)
+			{
+				printf("(nil)");
+				if (*(format + i + 1) != '\0')
+					printf(", ");
+				i++;
+				continue;
+			}
+			printf("%s", string);
 		}
 		if (*(format + i + 1) != '\0')
 			printf(", ");
