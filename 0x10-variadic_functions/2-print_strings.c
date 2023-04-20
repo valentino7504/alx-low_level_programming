2d12c4f76f0491484730670c56c6b5ff2a18bac3
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-20 20:50:16 +0100

    Resolved issues with task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,12 +10,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 	char *string;
-	
+
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(ap, char *);
-		printf("%s", string);
+		if (string == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", string);
+		}
 		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
