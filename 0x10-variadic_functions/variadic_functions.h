6b8f10dd539c3c78b705b73688d08dd762eb18fa
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-21 00:18:56 +0100

    Updated header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,10 @@ int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *format, ...);
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_str(va_list ap);
 /**
  * struct formats - operation structure
  *
