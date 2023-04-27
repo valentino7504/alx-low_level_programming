d5569e17dd92b15cf1f74ad0270e0986d6c59684
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-27 19:32:46 +0100

    Solved task 4 and updated lists.h

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees up a singly linked list
+ * @head: the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node = head;
+	list_t *next_node = head->next;
+
+	while (current_node)
+	{
+		next_node = current_node->next;
+		free(current_node->str);
+		free(current_node);
+		current_node = next_node;
+	}
+}
