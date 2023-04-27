41774c594658f1e876e4909e36c65d9a70483df2
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-27 18:28:59 +0100

    Solved task 1 and updated lists.h

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns the length of a linked list
+ * @h: the head of the list in question
+ * Return: the length of the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	const list_t *current_node = h;
+
+	while (current_node != NULL)
+	{
+		count++;
+		current_node = current_node->next;
+	}
+	return (count);
+}
