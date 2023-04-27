bd4afdb6a88226f8289fefb84f0fa9dbc3d690f5
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-27 21:01:15 +0100

    Extra task 2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,8 +2,8 @@
           extern    printf
 main:
 	  mov   edi, format
-	  xor   ebx, ebx
+	  xor   eax, eax
 	  call  printf
-	  mov 	ebx, 0
+	  mov 	eax, 0
 	  ret
 format: db `Hello, Holberton\n`,0
