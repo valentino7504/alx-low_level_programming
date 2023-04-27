60900b50ee0ca16364610e345d0f776a0d516bf6
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-27 20:30:08 +0100

    Fixed extra task 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-void print_text(void) __attribute__((constructor));
+void print_text(void) __attribute__ ((constructor));
 /**
  * print_text - prints a sentence before main
  */
 void print_text(void)
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
