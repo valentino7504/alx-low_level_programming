528cad4bb59b0dce46ccbab67228195dab22a074
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-29 22:35:07 +0100

    Fixed task 5 error 9

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 /**
- * free_listint2 - fress a list of integers
+ * free_listint2 - frees a list of integers
  * @head: the head of the list
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *current_node = *head;
-	listint_t *next_node;
 
 	if (*head == NULL || head == NULL)
 		return;
-	while (current_node != NULL)
+	current_node = current_node->next;
+	while (*head != NULL)
 	{
-		next_node = current_node->next;
+		current_node = *head;
+		*head = current_node->next;
 		free(current_node);
-		current_node = next_node;
 	}
 	*head = NULL;
 }
