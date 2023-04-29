e2171781d0273d5fd74210ded8419975b9f51c6e
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-29 22:51:24 +0100

    Added task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * pop_listint - pops th head element of a linked list
+ * and returns its value
+ * @head: the head of the linked list
+ * Return: the head's value or 0
+ */
+int pop_listint(listint_t **head)
+{
+	int value = 0;
+	listint_t *node = *head;
+
+	if (*head == NULL || head == NULL)
+		return (0);
+	value = (*head)->n;
+	if ((*head)->next == NULL)
+	{
+		free(*head);
+	}
+	else
+	{
+		*head = node->next;
+		free(node);
+	}
+	return(value);
+}
