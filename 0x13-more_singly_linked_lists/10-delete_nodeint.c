20c78b4203c37730c3ed12ac10e0ba6685858c0c
Edwin Ade <edwinmbonyjr@gmail.com>
2023-05-01 20:52:56 +0100

    Fixed betty on task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			prev_node->next = current_node->next;
 			free(current_node);
-			return (1); 
+			return (1);
 		}
 		prev_node = current_node;
 		current_node = current_node->next;
