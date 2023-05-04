c3353c3dd79bfa8516e229626cd4b2e33879048c
Edwin Ade <edwinmbonyjr@gmail.com>
2023-05-04 14:34:39 +0100

    Solved task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - sets a particular bit in a number to 0
+ * @n: the number
+ * @index: the index of the ibt to be cleared
+ * Return: 1 if succesful, 0 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * 8) - 1)
+		return (-1);
+	if (((*n >> index) & 1) == 0)
+		return (1);
+	*n -= 1 << index;
+	return (1); 
+}
