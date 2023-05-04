e09487ea58025bc7187e85efaf27ff744400c689
Edwin Ade <edwinmbonyjr@gmail.com>
2023-05-04 13:33:01 +0100

    Solved task 1 and updated main.h

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary number
+ * @n: the number in base 10
+ */
+void print_binary(unsigned long int n)
+{
+	int flag = 0;
+	unsigned long int i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = sizeof(unsigned long int) * 8; i > 0; i--)
+	{
+		if (n >> (i - 1) != 0)
+			flag = 1;
+		if (flag == 1)
+		{
+			_putchar(((n >> (i - 1)) & 1) + 48);
+		}
+	}
+}
