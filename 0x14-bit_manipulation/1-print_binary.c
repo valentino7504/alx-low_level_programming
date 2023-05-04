f6685313b050cb960972fa97f5511fa4e99f9b14
Edwin Ade <edwinmbonyjr@gmail.com>
2023-05-04 13:38:14 +0100

    Made code a bit more concise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,21 +5,18 @@
  */
 void print_binary(unsigned long int n)
 {
-	int flag = 0;
-	unsigned long int i = 0;
+	int flag = 0, i = 0;
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	for (i = sizeof(unsigned long int) * 8; i > 0; i--)
+	for (i = (int) sizeof(unsigned long int) * 8; i > 0; i--)
 	{
 		if (n >> (i - 1) != 0)
 			flag = 1;
 		if (flag == 1)
-		{
 			_putchar(((n >> (i - 1)) & 1) + 48);
-		}
 	}
 }
