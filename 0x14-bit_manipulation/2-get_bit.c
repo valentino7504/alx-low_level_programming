4e9a16484c9ec506bfd290076655588dc1bd3eed
Edwin Ade <edwinmbonyjr@gmail.com>
2023-05-04 13:57:41 +0100

    Fixed task 2 error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,8 +7,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int) * 8) - 1
-	|| index < 0)
+	if (index > (sizeof(unsigned long int) * 8) - 1)
 		return (-1);
 	return ((n >> index) & 1);
 }
