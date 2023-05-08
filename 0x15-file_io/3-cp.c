bd01d283411aefa8f79ac70fc44edb52855e8a03
Edwin Ade <edwinmbonyjr@gmail.com>
2023-05-09 00:35:00 +0100

    Solved task 3 issues£

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #define BUFFER_SIZE 1024
 /**
+ * print_error - Print error message to stderr and exit with code
+ * @errno: The exit code
+ * @message: The error message
+ * @arg: the arg
+ */
+void print_error(int errno, const char *message, char *arg)
+{
+	dprintf(STDERR_FILENO, message, arg);
+	exit(errno);
+}
+/**
  * main - creates a copy of the cp command
  * @argc: the number of command line arguments
  * @argv: the command line arguments
@@ -8,34 +19,25 @@
  */
 int main(int argc, char *argv[])
 {
-	int copied_file, new_file, read_bytes = 0;
+	int copied_file, new_file, read_bytes = 0, write_check;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		print_error(97, "Usage: cp file_from file_to", NULL);
 	copied_file = open(argv[1], O_RDONLY);
 	if (copied_file == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		print_error(98, "Can't read from file %s", argv[1]);
 	new_file = open(argv[2], O_TRUNC | O_RDWR | O_CREAT, 0664);
+	if (new_file == -1)
+		print_error(99, "Can't write to %s", argv[2]);
 	while ((read_bytes = read(copied_file, buffer, BUFFER_SIZE)) > 0)
 	{
-		if (write(new_file, buffer, read_bytes) == -1)
-		{
-			dprintf(2, "Can't write to %s", argv[2]);
-			exit(99);
-		}
+		write_check = write(new_file, buffer, read_bytes);
+		if (write_check == -1 || write_check != read_bytes)
+			print_error(99, "Error: Can't write to %s", argv[2]);
 	}
-	if (read_bytes == -1 || new_file == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	if (read_bytes == -1)
+		print_error(98, "Can't read from file %s", argv[1]);
 	if (close(new_file) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", new_file);
