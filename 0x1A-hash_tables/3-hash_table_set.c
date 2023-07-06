02210630916e6589d127aefbada1976ad1be2674
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-06 23:56:07 +0100

    Solved task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - sets a key value pair in a hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ * Return: 0 if failed or 1 if succeeded
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index((unsigned char *) key, ht->size);
+	hash_node_t *new_node;
+
+	if (strlen(key) == 0)
+		return (0);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = malloc(sizeof(char *));
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	strcpy(new_node->key, key);
+	new_node->value = malloc(sizeof(char *));
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	strcpy(new_node->value, value);
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_node;
+		return (1);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
