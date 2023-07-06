952e0f802ba93247230633061de81d3c4db832f9
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-06 23:06:08 +0100

    Fixed creation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,14 +7,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 	
 	if (new_table == NULL)
 		return (NULL);
-	new_table->array = malloc(size * sizeof(hash_node_t));
+	new_table->array = malloc(size * sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 	return (new_table);
 }
