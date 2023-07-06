66820d4890424558921bde7e271c259edb2882a8
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-06 23:57:12 +0100

    Fixed NULL issue

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,6 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new_node;
+		new_node->next = NULL;
 		return (1);
 	}
 	new_node->next = ht->array[index];
