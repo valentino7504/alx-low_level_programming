4cb22cbdc91dd36b956b338d0bac29e3a55dd192
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-06 23:08:39 +0100

    Fixed task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_table_create: creates an empty hash table
+ * hash_table_create - creates an empty hash table
  * @size: the size of the hash table array
  * Return: a pointer to the new tables
  */
@@ -8,9 +8,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 	unsigned long int i;
-	
+
 	if (new_table == NULL)
 		return (NULL);
+	new_table->size = size;
 	new_table->array = malloc(size * sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
