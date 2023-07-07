110354f3d226fedcc884541241110f9789cbc427
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-07 12:50:01 +0100

    Fixed 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,17 +8,25 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((unsigned char *) key, ht->size);
+	unsigned long int index = key_index((unsigned char *) key, ht->size), i;
 	char *new_value;
 	hash_node_t *new_node = malloc(sizeof(hash_node_t)), *current_node;
 
-	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0
-	|| *key == '\0')
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
 	current_node = ht->array[index];
 	new_value = strdup(value);
 	if (new_value == NULL)
 		return (0);
+	for (i = 0; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = new_value;
+			return (1);
+		}
+	}
 	while (current_node != NULL)
 	{
 		if (strcmp(current_node->key, key) == 0)
