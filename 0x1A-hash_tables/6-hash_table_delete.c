c3f1378a9d0d10570e13e104dbe2be59b5f59398
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-07 19:36:42 +0100

    Solved task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - frees a hash tables
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *current_node, *next_node;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node != NULL)
+		{
+			next_node = current_node->next;
+			free(current_node->key);
+			free(current_node->value);
+			free(current_node);
+			current_node = next_node;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
