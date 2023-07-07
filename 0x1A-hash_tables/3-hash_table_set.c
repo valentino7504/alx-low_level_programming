bdd150f428fa65d31a36a091ff75d556d5ad97e1
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-07 12:58:07 +0100

    Fixed null ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,23 +8,27 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *) key, ht->size), i;
+	unsigned long int index;
 	char *new_value;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node = malloc(sizeof(hash_node_t)), *current_node;
 
-	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0
+	|| *key == '\0')
 		return (0);
+	index = key_index((const unsigned char *) key, ht->size);
+	current_node = ht->array[index];
 	new_value = strdup(value);
 	if (new_value == NULL)
 		return (0);
-	for (i = index; ht->array[i]; i++)
+	while (current_node != NULL)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(current_node->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = new_value;
+			free(current_node->value);
+			current_node->value = new_value;
 			return (1);
 		}
+		current_node = current_node->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
