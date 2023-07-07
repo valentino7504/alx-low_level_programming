823b7aa20eb18479185cd417a81030f1383e6499
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-07 12:42:56 +0100

    Fixed task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,37 +9,35 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((unsigned char *) key, ht->size);
+	char *new_value;
 	hash_node_t *new_node = malloc(sizeof(hash_node_t)), *current_node;
 
-	if (strlen(key) == 0 || new_node == NULL || !key || !value)
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-		return (0);
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-		return (0);
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-		new_node->next = NULL;
-		return (1);
-	}
 	current_node = ht->array[index];
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
 	while (current_node != NULL)
 	{
-		if (strcmp(current_node->key, new_node->key) == 0)
+		if (strcmp(current_node->key, key) == 0)
 		{
-			current_node->value = strdup(new_node->value);
-			if (current_node->value == NULL)
-				return (0);
-			free(new_node->key);
-			free(new_node->value);
-			free(new_node);
+			free(current_node->value);
+			current_node->value = new_value;
 			return (1);
 		}
 		current_node = current_node->next;
 	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = new_value;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
