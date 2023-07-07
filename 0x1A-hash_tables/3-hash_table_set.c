b961d9f743801a1762234c0e84f012da811d1192
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-07 12:53:48 +0100

    Solved task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,15 +10,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((unsigned char *) key, ht->size), i;
 	char *new_value;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t)), *current_node;
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
-	current_node = ht->array[index];
 	new_value = strdup(value);
 	if (new_value == NULL)
 		return (0);
-	for (i = 0; ht->array[i]; i++)
+	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
@@ -26,16 +25,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			ht->array[i]->value = new_value;
 			return (1);
 		}
-	}
-	while (current_node != NULL)
-	{
-		if (strcmp(current_node->key, key) == 0)
-		{
-			free(current_node->value);
-			current_node->value = new_value;
-			return (1);
-		}
-		current_node = current_node->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
